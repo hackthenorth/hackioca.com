e786177877f7384d5fdc82094fa749743bf07f4a
Jevin Sidhu <jevin.sidhu@gmail.com>
2019-03-25 10:19:49 -0400

    About Section (#4)
    * Add about section
    
    * Use media templates and fix nit-picks
    
    * Add title to copy file
    
    * Change from converting to em to just using px

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,40 @@
 import React from "react";
-import HelloWorldTestState from "src/app/components/HelloWorldTestState";
+import { createGlobalStyle } from "styled-components";
 import { BobaProvider } from "src/utils/context/boba";
 
-const App: React.FC<{}> = () => (
+import About from "src/components/About";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Bubbleboddy";
+    font-weight: 700;
+    src: url("/fonts/Bubbleboddy-FatTrial.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Raleway";
+    font-weight: 400;
+    src: url("/fonts/Raleway-Light.tff") format("truetype");
+  }
+
+  html {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background: linear-gradient(180deg, #E5C49E 0%, #FFFFFF 100%);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const App: React.FC = () => (
   <BobaProvider>
-    <HelloWorldTestState />
+    <About />
+    <GlobalStyle />
   </BobaProvider>
 );
-
 export default App;
---
+++ src/components/About/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import copy from "src/copy";
+import media from "src/utils/media";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Section = styled.div`
+  display: flex;
+
+  ${media.phone`
+    flex-direction: column;
+    align-items: center;
+    `}
+`;
+
+const Header = styled.h1`
+  font-family: "Bubbleboddy";
+  font-size: 48px;
+  color: #000000;
+  margin-bottom: 5px;
+
+  ${media.phone`
+    text-align: center;
+  `}
+`;
+
+const Paragraph = styled.p`
+  font-family: "Raleway";
+  font-size: 16px;
+  line-height: 1.5em;
+  color: #51192c;
+  width: 275px;
+  margin-right: 50px;
+
+  ${media.phone`
+    text-align: center;
+    margin-right: 0;
+    width: 280px;
+  `}
+`;
+
+const Image = styled.img`
+  width: 175px;
+  height: 185px;
+
+  ${media.phone`
+    width: 145px;
+    height: 155px;
+  `}
+`;
+
+const About: React.FC = () => (
+  <Wrapper>
+    <div>
+      <Header>{copy.about.title}</Header>
+      <Section>
+        <Paragraph>{copy.about.desc}</Paragraph>
+        <Image src="images/boiling_boba_about.gif" />
+      </Section>
+    </div>
+  </Wrapper>
+);
+
+export default About;
