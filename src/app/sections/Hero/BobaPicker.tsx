c52c6df6b6065e99e6d39580af2b0e1f668c3f76
Alex Xie <alexieyizhe@gmail.com>
2019-03-26 01:07:59 -0400

    Get arrows working

--- src/app/sections/Hero/BobaPicker.tsx
+++ src/app/sections/Hero/BobaPicker.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { useBobaContext } from "src/utils/context/boba";
 import { flavors, toppings, Flavor, Topping } from "src/siteData";
-import PlaceholderBobaImg from "src/static/images/hero/hero_mango_boba.svg";
+
+import ImgChevron from "src/static/images/chevron_up.svg";
+import ImgBobaPlaceholder from "src/static/images/hero/hero_mango_boba.svg";
 
 
+/* HELPER FUNCTIONS */
 const isTopping = (option: Topping | Flavor): boolean => toppings.indexOf(option as Topping) >= 0;
 
-
 // https://stackoverflow.com/questions/50924952/typescript-has-no-compatible-call-signatures
-const showOptions = (selected: Topping | Flavor): (Topping | Flavor)[] => {
+const filterShownOptions = (selected: Topping | Flavor): (Topping | Flavor)[] => {
   let resultArr = (isTopping(selected) ? [...toppings] : [...flavors]) as (Topping | Flavor)[];
   const posInArr = resultArr.indexOf(selected);
 
@@ -26,6 +28,20 @@ const showOptions = (selected: Topping | Flavor): (Topping | Flavor)[] => {
   return resultArr;
 }
 
+const shiftOptionBy = (selected: Topping | Flavor, shiftBy: number): (Topping | Flavor) => {
+  let newIndex = (isTopping(selected) ? toppings.indexOf(selected as Topping) : flavors.indexOf(selected as Flavor)) + shiftBy;
+
+  if(newIndex < 0) {
+    newIndex = (toppings.length + shiftBy);
+  } else if(newIndex >= toppings.length) {
+    newIndex = 0 + (shiftBy - 1);
+  }
+
+  return (isTopping(selected) ? toppings[newIndex] : flavors[newIndex]);
+}
+
+
+/* STYLED COMPONENTS */
 const Container = styled.div`
   width: 550px;
   height: 375px;
@@ -70,11 +86,12 @@ const Picker = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
+  margin: 20px auto;
 `;
 
 const PickerOption = styled.div<{ selected: boolean }>`
-  width: 70px;
-  height: 70px;
+  width: 60px;
+  height: 60px;
 
   border-radius: 50%;
   border: 3px solid white;
@@ -89,31 +106,44 @@ const PickerOption = styled.div<{ selected: boolean }>`
 `;
 
 
+const Arrow = styled.img`
+  max-width: 30px;
+  max-height: 30px;
+
+  margin: 0 auto;
+`;
 
 
+/* MAIN COMPONENT */
 const MailingListSignup: React.FC = () => {
 
   const { flavor: selectedFlavor, topping: selectedTopping, updateFlavor, updateTopping } = useBobaContext();
-
-  const shownFlavors = showOptions(selectedFlavor) as Flavor[];
-  const shownToppings = showOptions(selectedTopping) as Topping[];
-
+  const shownFlavors = filterShownOptions(selectedFlavor) as Flavor[];
+  const shownToppings = filterShownOptions(selectedTopping) as Topping[];
 
   return (
     <Container>
       <Picker>
+        <Arrow src={ImgChevron} onClick={() => updateFlavor(shiftOptionBy(selectedFlavor, -1) as Flavor)} />
         {shownFlavors.map(flavor => (
-          <PickerOption onClick={() => updateFlavor(flavor)} selected={flavor === selectedFlavor}>{flavor}</PickerOption>
+          <PickerOption onClick={() => updateFlavor(flavor)} selected={flavor === selectedFlavor}>
+            {flavor}
+          </PickerOption>
         ))}
+        <Arrow src={ImgChevron} onClick={() => updateFlavor(shiftOptionBy(selectedFlavor, 1) as Flavor)} />
       </Picker>
       <Boba>
-        <img src={PlaceholderBobaImg} />
+        <img src={ImgBobaPlaceholder} />
         <div className="circleBg" />
       </Boba>
       <Picker>
+        <Arrow src={ImgChevron} onClick={() => updateTopping(shiftOptionBy(selectedTopping, -1) as Topping)} />
         {shownToppings.map(topping => (
-          <PickerOption onClick={() => updateTopping(topping)} selected={topping === selectedTopping}>{topping}</PickerOption>
+          <PickerOption onClick={() => updateTopping(topping)} selected={topping === selectedTopping}>
+            {topping}
+          </PickerOption>
         ))}
+        <Arrow src={ImgChevron} onClick={() => updateTopping(shiftOptionBy(selectedTopping, 1) as Topping)} />
       </Picker>
     </Container>
   );
