83a468d8128b5d92ccb79001f8ce625f9d9e77c6
Kevin Pei <kevin-pei@hotmail.com>
2019-03-28 23:55:43 -0400

    Only enable bugsnag on production

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import bugsnagReact from "@bugsnag/plugin-react";
 
 const bugsnagClient = bugsnag('9d3336190be1523df42e4d783418e856');
 bugsnagClient.use(bugsnagReact, React);
-const ErrorBoundary = bugsnagClient.getPlugin('react');
+const ErrorBoundary = process.env.NODE_ENV === "production" ? bugsnagClient.getPlugin('react') : React.Fragment;
 
 ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById("root"));
 
