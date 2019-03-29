dd3a864d9452808b99733aeea9c62f2b1b72b9ec
Kevin Pei <kevin-pei@hotmail.com>
2019-03-28 23:47:20 -0400

    Add Google Analytics and Bugsnag

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./app/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Bugsnag
+import bugsnag from "@bugsnag/js";
+import bugsnagReact from "@bugsnag/plugin-react";
+
+const bugsnagClient = bugsnag('9d3336190be1523df42e4d783418e856');
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin('react');
+
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById("root"));
 
 registerServiceWorker();
