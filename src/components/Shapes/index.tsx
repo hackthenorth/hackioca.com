93d9a805dcbbeba2044f628b32e3d1333e10d63a
Jevin Sidhu <jevin.sidhu@gmail.com>
2019-03-31 14:11:28 -0400

    Initial attempt

--- src/components/Shapes/index.tsx
+++ src/components/Shapes/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Shape, { ShapeProps } from "src/components/Shape";
+import posed, { PoseGroup } from "react-pose";
 
 interface ShapesProps {
   shapes: ShapeProps[];
 }
 
+const Item = posed.div({
+  enter: { opacity: 1 },
+  exit: { opacity: 0 }
+});
+
 const Shapes = ({ shapes }: ShapesProps) => (
-  <>
+  <PoseGroup>
     {shapes.map(({ top, left, scale, angle }) => (
-      <Shape
-        key={`${top}-${left}-${scale}-${angle}`}
-        top={top}
-        left={left}
-        scale={scale}
-        angle={angle}
-      />
+      <Item key={`${top}-${left}-${scale}-${angle}`}>
+        <Shape
+          key={`${top}-${left}-${scale}-${angle}`}
+          top={top}
+          left={left}
+          scale={scale}
+          angle={angle}
+        />
+      </Item>
     ))}
-  </>
+  </PoseGroup>
 );
 
 export default React.memo(Shapes);
